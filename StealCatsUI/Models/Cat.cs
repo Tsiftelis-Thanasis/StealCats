6dc081eabcac8546d8cb2a81419593bd9aa0a106
Thanasis Tsiftelis <tsiftelis.thanasis@gmail.com>
2025-03-09 23:49:05 +0200

    Split models

--- StealCatsUI/Models/Cat.cs
+++ StealCatsUI/Models/Cat.cs
@@ -12,11 +12,5 @@
         public ICollection<TagEntity> tags { get; set; } = new List<TagEntity>();
     }
 
-    public class TagEntity
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public DateTime created { get; set; } = DateTime.UtcNow;
-        public ICollection<CatEntity> cats { get; set; } = new List<CatEntity>();
-    }
+   
 }
---
+++ StealCatsUI/Models/TagEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace StealCatsUI.Models
+{
+    public class TagEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime created { get; set; } = DateTime.UtcNow;
+        public ICollection<CatEntity> cats { get; set; } = new List<CatEntity>();
+    }
+}
